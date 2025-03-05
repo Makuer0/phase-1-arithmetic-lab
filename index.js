a42f5232c77a81a74922281d6ece8882f75de470
Makuer <makuer.mqtur@student.moringaschool.com>
2025-03-05 10:40:37 +0300

    arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+
+let num3 = 10;
+let num4 = 6;
+
+
+
+let multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 5);
+
+let mod = num3 % num4;
+
+let set = [5,6,7,20];
+let max = Math.max(...set);
